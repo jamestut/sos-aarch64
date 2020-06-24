32adf1f1bf5abc72f3f78fac4bfe19a14433d6ff
James <j.nugraha@student.unsw.edu.au>
2020-06-25 09:32:30 +1000

    The missing pieces in mmap support @ user app.

--- projects/aos/libsosapi/include/sossysnr.h
+++ projects/aos/libsosapi/include/sossysnr.h
@@ -2,10 +2,11 @@
 
 #define SOS_SYSCALL_UNIMPLEMENTED 0x7FFFFFFF
 
-#define SOS_SYSCALL_READ  0
-#define SOS_SYSCALL_WRITE 1
-#define SOS_SYSCALL_OPEN  2
-#define SOS_SYSCALL_CLOSE 3
-#define SOS_SYSCALL_BRK   12
-#define SOS_SYSCALL_USLEEP 35 // equivalent to linux's nanosleep
+#define SOS_SYSCALL_READ        0
+#define SOS_SYSCALL_WRITE       1
+#define SOS_SYSCALL_OPEN        2
+#define SOS_SYSCALL_CLOSE       3
+#define SOS_SYSCALL_MMAP        9
+#define SOS_SYSCALL_BRK        12
+#define SOS_SYSCALL_USLEEP     35 // equivalent to linux's nanosleep
 #define SOS_SYSCALL_TIMESTAMP 201 // linux's time 
--- projects/aos/libsosapi/src/sys_moremem.c
+++ projects/aos/libsosapi/src/sys_moremem.c
@@ -1,8 +1,7 @@
-#ifndef ENABLED
-
 #include <stdint.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <sys/types.h>
 #include <sos.h>
 #include <sel4/sel4.h>
 
@@ -10,7 +9,9 @@
 
 long sys_brk(va_list ap)
 {
+    // params
     size_t newbrk = va_arg(ap, size_t);
+
     seL4_MessageInfo_t msginfo = seL4_MessageInfo_new(0, 0, 0, 2);
     seL4_SetMR(0, SOS_SYSCALL_BRK);
     seL4_SetMR(1, newbrk);
@@ -24,4 +25,27 @@ long sys_brk(va_list ap)
     return seL4_GetMR(0);
 }
 
-#endif
+long sys_mmap(va_list ap)
+{
+    // params
+    void *addr = va_arg(ap, void *);
+    size_t length = va_arg(ap, size_t);
+    int prot = va_arg(ap, int);
+    int flags = va_arg(ap, int);
+    int fd = va_arg(ap, int);
+    off_t offset = va_arg(ap, off_t);
+
+    seL4_MessageInfo_t msginfo = seL4_MessageInfo_new(0, 0, 0, 7);
+    seL4_SetMR(0, SOS_SYSCALL_MMAP);
+    seL4_SetMR(1, addr);
+    seL4_SetMR(2, length);
+    seL4_SetMR(3, prot);
+    seL4_SetMR(4, flags);
+    seL4_SetMR(5, fd);
+    seL4_SetMR(6, offset);
+    
+    msginfo = seL4_Call(SOS_IPC_EP_CAP, msginfo);
+
+    // negative errno semantic, but we return to caller as-is
+    return seL4_GetMR(0);
+}
